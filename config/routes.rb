bb896471b609a969e664f84dcd442e0551393670
linh trieu <linhtrieu7695@gmail.com>
2022-03-01 17:11:42 +0100

    cloudinary in production.rb and add a group controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
   resources :groups
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-=======
   get "/ui_kit", to: "pages#ui_kit"
->>>>>>> master
 end
