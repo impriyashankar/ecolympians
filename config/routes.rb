804ab48fca7e9c67f4f19c998537226acac06518
Ofer Peled <ofer2406@gmail.com>
2022-03-02 17:37:25 +0100

    top part and tabs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :groups
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
   get "/ui_kit", to: "pages#ui_kit"
 end
