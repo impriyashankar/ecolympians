e4c24ab0a890f24370ecbb098da11a4fab1f1c93
priya shankar <preiyah@gmail.com>
2022-03-03 14:59:17 +0100

    handled scenario for multiple grp challenges

--- app/controllers/group_challenges_controller.rb
+++ app/controllers/group_challenges_controller.rb
@@ -2,7 +2,8 @@ class GroupChallengesController < ApplicationController
 
   def index
     @user = current_user
-    @group_challenge = GroupChallenge.find(params[:group_id])
+    #@group_challenge = GroupChallenge.find(params[:group_id])
+    @group_challenge = GroupChallenge.where("membership_id = ?",params[:group_id])
     @group = Group.find(params[:group_id])
     #raise
   end
