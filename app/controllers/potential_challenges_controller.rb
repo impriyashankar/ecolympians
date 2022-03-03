3862bd1e501d9094913f33ddf7af653eb73b407d
Ofer Peled <ofer2406@gmail.com>
2022-03-03 17:50:33 +0100

    votes sum

--- app/controllers/potential_challenges_controller.rb
+++ app/controllers/potential_challenges_controller.rb
@@ -1,9 +1,23 @@
 class PotentialChallengesController < ApplicationController
 
   def create
+    @group = Group.find(params[:id])
+    if @group.potential_challenges.count == 3
+      @group.potential_challenges.destroy_all
+    end
     @p_challenge = PotentialChallenge.new(p_challenge_params)
-    @p_challenge.group = Group.find(params[:id])
+  end
 
+  def votes_tally
+    sum = 0
+    challenge_votes.each do |challenge_vote|
+      if challenge_vote.vote?
+        sum -= 1
+      else
+        sum += 1
+      end
+    end
+    return sum
   end
 
   private
