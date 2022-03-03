9e6fdf5fd92fee56d2d39ccc44dde451a78849e3
Ofer Peled <ofer2406@gmail.com>
2022-03-03 17:28:01 +0100

    started create method for votes

--- app/controllers/potential_challenges_controller.rb
+++ app/controllers/potential_challenges_controller.rb
@@ -1,2 +1,14 @@
 class PotentialChallengesController < ApplicationController
+
+  def create
+    @p_challenge = PotentialChallenge.new(p_challenge_params)
+    @p_challenge.group = Group.find(params[:id])
+
+  end
+
+  private
+
+  def p_challenge_params
+    params.require(:potential_challenge).permit(:challenge_id)
+  end
 end
