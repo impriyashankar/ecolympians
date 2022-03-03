79511db75325f2f410a39548a30baf9d775cccb6
priya shankar <preiyah@gmail.com>
2022-03-03 18:52:29 +0100

    minor changes for mobile view

--- app/controllers/group_challenges_controller.rb
+++ app/controllers/group_challenges_controller.rb
@@ -3,7 +3,7 @@ class GroupChallengesController < ApplicationController
   def index
     @user = current_user
     #@group_challenge = GroupChallenge.find(params[:group_id])
-    @group_challenge = GroupChallenge.where("membership_id = ?",params[:group_id])
+    #@group_challenge = GroupChallenge.where("membership_id = ?",params[:group_id]) -- moved to groups_controller
     @group = Group.find(params[:group_id])
     #raise
   end
