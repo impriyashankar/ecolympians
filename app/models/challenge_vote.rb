5544fba36ac4a63c5decf0d4226291fb4d6c2c3d
Ofer Peled <ofer2406@gmail.com>
2022-03-04 14:42:02 +0100

    semicolon to colon

--- app/models/challenge_vote.rb
+++ app/models/challenge_vote.rb
@@ -4,6 +4,6 @@ class ChallengeVote < ApplicationRecord
   validates :vote, presence: true
 
   def create
-    @potential_challenge = PotentialChallenge.find(params[;potential_challenge_id])
+    @potential_challenge = PotentialChallenge.find(params[:potential_challenge_id])
   end
 end
