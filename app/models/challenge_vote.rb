ca2649f7a3991a71eeb32d7adb974de48b31108b
Ofer Peled <ofer2406@gmail.com>
2022-03-04 14:34:36 +0100

    challenge card

--- app/models/challenge_vote.rb
+++ app/models/challenge_vote.rb
@@ -2,4 +2,8 @@ class ChallengeVote < ApplicationRecord
   belongs_to :potential_challenge
   belongs_to :user
   validates :vote, presence: true
+
+  def create
+    @potential_challenge = PotentialChallenge.find(params[;potential_challenge_id])
+  end
 end
