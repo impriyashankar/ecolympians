104b5669202b07b24fed04edc912da1f1127aa50
linh trieu <linhtrieu7695@gmail.com>
2022-03-04 15:17:26 +0100

    its for now

--- app/controllers/potential_challenges_controller.rb
+++ app/controllers/potential_challenges_controller.rb
@@ -1,13 +1,20 @@
 class PotentialChallengesController < ApplicationController
   def create
+    # potential_challenges = []
+    # potential_challenges_ids = params["potential_challenge"]["challenge_id"]
+    # potential_challenges_ids.delete_at(0)
+    # potential_challenges_ids.each do |id|
+    #  po = PotentialChallenge.new(group_id: params[:group_id].to_i, challenge_id: id)
+    #  po.save
+    #  potential_challenges << po
+    # end
+
     potential_challenges = []
-    potential_challenges_ids = params["potential_challenge"]["challenge_id"]
-    potential_challenges_ids.delete_at(0)
-    potential_challenges_ids.each do |id|
-     po = PotentialChallenge.new(group_id: params[:group_id].to_i, challenge_id: id)
-     po.save
-     potential_challenges << po
+    challenges_names = [ params[:challenge1], params[:challenge2], params[:challenge3] ]
+    challenges_names.each do |cn|
+      potential_challenges << PotentialChallenge.create!(challenge: Challenge.find_by(name: cn), group_id: params[:group_id].to_i)
     end
+
     if potential_challenges.last.save
       redirect_to group_path(params[:group_id])
     else
