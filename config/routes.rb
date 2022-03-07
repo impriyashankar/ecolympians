ef5572077b6ca01f3b109a7edde1c0da167bea2f
Ofer Peled <ofer2406@gmail.com>
2022-03-07 09:59:46 +0100

    fixed routing error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, except: [:show]
   root to: 'pages#home'
   resources :groups do
-    resources :group_challenges, only: [:new, :create, :index, :show]
+    resources :group_challenges, only: [:new, :create, :index, :show, :update]
     resources :memberships, only: [:new, :create, :index, :show]
     resources :potential_challenges, only: [:create] do
       resources :challenge_votes, only: [:create]
@@ -13,6 +13,5 @@ Rails.application.routes.draw do
   get "/ui_kit", to: "pages#ui_kit"
   resources :challenges, only: [:index, :show, :new, :create]
 
-  resources :group_challenges, only: [:create, :update]
   get "/my_dashboard", to: "users#show"
 end
