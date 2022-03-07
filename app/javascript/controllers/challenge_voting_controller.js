40d78721f6ff7da94258086d41fe8fdbbbab65ba
Ofer Peled <ofer2406@gmail.com>
2022-03-07 14:03:36 +0100

    button disappears

--- app/javascript/controllers/challenge_voting_controller.js
+++ app/javascript/controllers/challenge_voting_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["output"]
+  static targets = ["button"]
 
   reloadContent(event) {
     event.preventDefault();
+    this.buttonTargets.forEach((button) => {
+      button.classList.add("d-none");
+    });
   }
 }
