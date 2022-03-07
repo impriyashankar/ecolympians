18522c1c467ea001c59afa7a7424821ac933232f
Ofer Peled <ofer2406@gmail.com>
2022-03-07 17:50:36 +0100

    added destroy dependency for group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
   has_many :group_challenges, through: :memberships
-  has_many :potential_challenges
+  has_many :potential_challenges, dependent: :destroy
   validates :name, presence: true
 
   def vote_ended?
