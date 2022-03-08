b350eeac66b6c4212889fd17ec636755e83916cf
priya shankar <preiyah@gmail.com>
2022-03-08 12:18:07 +0100

    view and join other groups, and pending inv fix on dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-    @groups = @user.groups
+    #@groups = @user.groups -- old logic
+    @groups = @user.memberships.where(status: "Accepted") # Changed logic to show only 'Accepted' status groups
     @group = Group.new
     @group_challenges_ongoing = @user.group_challenges.select{ |challenge| challenge.status == "ongoing" }
     @group_challenges_upcoming = @user.group_challenges.select{ |challenge| challenge.status == "waiting" }
