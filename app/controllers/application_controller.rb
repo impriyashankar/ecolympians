43f454263f70de9305201c5c0e9463aeb4dccd99
Ofer Peled <ofer2406@gmail.com>
2022-03-08 15:03:37 +0100

    rules automatically show after signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    my_dashboard_path
+    my_dashboard_path(new_user: true)
   end
 
   protected
