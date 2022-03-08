78bdb5ed7c4982dec4ec7c3472ca94b49e510d23
priya shankar <preiyah@gmail.com>
2022-03-08 16:49:28 +0100

    changed stylesheet pack tag

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  config.serve_static_files = true
+  config.serve_static_files = true #added this to fix select2 form
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
